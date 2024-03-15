c2cc14d0f44e55c2fe928a730f947caafa28ea8c
Jose Rotchen <jose.rotchen14@gmail.com>
2024-03-15 19:02:15 -0300

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+const luisitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
+
+export { inter, luisitana };
